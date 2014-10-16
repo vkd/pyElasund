709259887d165790d359b688ea69022f9e09c33a
vkd <vkd.castle@gmail.com>
2014-10-17 00:05:18 +0400

    add fonts

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ def get_cell(x, y):
 	border = 1
 	return (start[0] + y * (step + border), start[1] + x * (step + border))
 
-font = pygame.font.Font(None, 24)
+font = pygame.font.Font("../fonts/Miramob.ttf", 16)
 
 count_players = 4
 runing = True
@@ -39,13 +39,14 @@ while runing:
 	windowRootSurface.fill(colors.PURPLE)
 	windowRootSurface.blit(tm.textures['board'], (0, 0))
 
-	for i in range(11):
-		pygame.draw.line(windowRootSurface, colors.WHITE, get_cell(i, 0), get_cell(i, 9))
-	for i in range(10):
-		pygame.draw.line(windowRootSurface, colors.WHITE, get_cell(0, i), get_cell(10, i))
+	# for i in range(11):
+	# 	pygame.draw.line(windowRootSurface, colors.WHITE, get_cell(i, 0), get_cell(i, 9))
+	# for i in range(10):
+	# 	pygame.draw.line(windowRootSurface, colors.WHITE, get_cell(0, i), get_cell(10, i))
+
 	for i in range(10):
 		for j in range(9):
-			windowRootSurface.blit(font.render('(%s, %s)' % (i, j), 1, colors.BLUE), tuple(i + 5 for i in get_cell(i, j)))
+			windowRootSurface.blit(font.render('(%s,%s)' % (i, j), 1, colors.BLUE), tuple(i + 5 for i in get_cell(i, j)))
 
 	windowRootSurface.blit(tm.textures['corner_top'], get_cell(-1, 4 + (count_players - 2) * 2))
 	windowRootSurface.blit(tm.textures['corner_bottom'], get_cell(9, 4 + (count_players - 2) * 2))
