f3b6db6c7146eae5042b034569208e934e199d57
vkd <vkd.castle@gmail.com>
2015-09-02 23:00:08 +0300

    refactor texture_manager

--- src/managers/texture_manager.py
+++ src/managers/texture_manager.py
@@ -1,14 +1,13 @@
 import pygame
+import os
 
 
-class TextureManager():
-    path = ''
+class TextureManager(object):
     textures = {}
 
     def __init__(self, path):
         self.path = path
 
-        folder = 'Buildings/'
         colors = {
             0: 'red',
             1: 'green',
@@ -16,51 +15,58 @@ class TextureManager():
             3: 'yellow'
         }
 
-        self.textures['building'] = {
-            'church': {i: self.load_texture('%sChurch%s' % (folder, i)) for i in range(1, 10)},
-            'draw_well': self.load_texture('%sDrawWell' % folder),
-            'fair': self.load_texture('%sFair' % folder),
-            'government': {i: self.load_texture('%sGovernment%s' % (folder, i)) for i in range(1, 4)},
-            'hotel': self.load_texture('%sHotel' % folder),
-            'house': {colors[i]: self.load_texture('%sHouse%s' % (folder, i)) for i in range(4)},
-            'shop': self.load_texture('%sShop' % folder),
-            'small_totem': {colors[i]: self.load_texture('%sSmallTotem%s' % (folder, i)) for i in range(4)},
-            'totem': {colors[i]: self.load_texture('%sTotem%s' % (folder, i)) for i in range(4)},
-            'workshop': {colors[i]: self.load_texture('%sWorkShop%s' % (folder, i)) for i in range(4)}
+        folder = 'Buildings'
+        self['building'] = {
+            'church': {i: self.load('Church%s.png' % i, folder) for i in range(1, 10)},
+            'draw_well': self.load('DrawWell.png', folder),
+            'fair': self.load('Fair.png', folder),
+            'government': {i: self.load('Government%s.png' % i, folder) for i in range(1, 4)},
+            'hotel': self.load('Hotel.png', folder),
+            'house': {colors[i]: self.load('House%s.png' % i, folder) for i in range(4)},
+            'shop': self.load('Shop.png', folder),
+            'small_totem': {colors[i]: self.load('SmallTotem%s.png' % i, folder) for i in range(4)},
+            'totem': {colors[i]: self.load('Totem%s.png' % i, folder) for i in range(4)},
+            'workshop': {colors[i]: self.load('WorkShop%s.png' % i, folder) for i in range(4)}
         }
 
-        folder = 'Claims/'
-        self.textures['claim'] = {
-            colors[c]: {i: self.load_texture('%s%s%s' % (folder, colors[c][:1], i)) for i in range(5)} for c in range(4)
+        folder = 'Claims'
+        self['claim'] = {
+            colors[c]: {i: self.load('%s%s.png' % (colors[c][:1], i), folder) for i in range(5)} for c in range(4)
         }
 
-        folder = 'Helps/'
-        self.textures['help'] = {
-            colors[i]: self.load_texture('%sHelp%s%s' % (folder, colors[i][:1].upper(), colors[i][1:])) for i in range(4)
+        folder = 'Helps'
+        self['help'] = {
+            colors[i]: self.load('Help%s%s.png' % (colors[i][:1].upper(), colors[i][1:]), folder) for i in range(4)
         }
 
-        folder = 'Walls/'
-        self.textures['wall'] = {
-            colors[c]: self.load_texture('%sWall%s%s' % (folder, colors[c][:1].upper(), colors[c][1:])) for c in range(4)
+        folder = 'Walls'
+        self['wall'] = {
+            colors[c]: self.load('Wall%s%s.png' % (colors[c][:1].upper(), colors[c][1:]), folder) for c in range(4)
         }
-        self.textures['wall'][1] = self.load_texture('%sWall1' % folder)
-        self.textures['wall'][2] = self.load_texture('%sWall2' % folder),
+        self['wall'][1] = self.load('Wall1.png', folder)
+        self['wall'][2] = self.load('Wall2.png', folder),
 
-        self.textures['board'] = self.load_texture('Board')
-        self.textures['church_token'] = self.load_texture('Church0')
-        self.textures['corner_top'] = self.load_texture('Corner_top')
-        self.textures['corner_bottom'] = self.load_texture('Corner_bottom')
-        self.textures['cube'] = self.load_texture('Cube')
-        self.textures['cursor'] = self.load_texture('Cursor_sm')
-        self.textures['gold'] = self.load_texture('Gold')
-        self.textures['icon'] = self.load_texture('icon')
-        self.textures['point'] = self.load_texture('Point')
-        self.textures['ship'] = self.load_texture('Ship')
-        self.textures['vote'] = {
-            'blue': self.load_texture('VoteBlue'),
-            'green': self.load_texture('VoteGreen'),
-            'red': self.load_texture('VoteRed')
+        self['board'] = self.load('Board.png')
+        self['church_token'] = self.load('Church0.png')
+        self['corner_top'] = self.load('Corner_top.png')
+        self['corner_bottom'] = self.load('Corner_bottom.png')
+        self['cube'] = self.load('Cube.png')
+        self['cursor'] = self.load('cursor_sm.png')
+        self['gold'] = self.load('Gold.png')
+        self['icon'] = self.load('icon.png')
+        self['point'] = self.load('Point.png')
+        self['ship'] = self.load('Ship.png')
+        self['vote'] = {
+            'blue': self.load('VoteBlue.png'),
+            'green': self.load('VoteGreen.png'),
+            'red': self.load('VoteRed.png')
         }
 
-    def load_texture(self, name):
-        return pygame.image.load('{0}{1}.png'.format(self.path, name)).convert_alpha()
+    def __getitem__(self, name):
+        return self.textures[name]
+
+    def __setitem__(self, name, value):
+        self.textures[name] = value
+
+    def load(self, name, path=''):
+        return pygame.image.load(os.path.join(self.path, path, name)).convert_alpha()
